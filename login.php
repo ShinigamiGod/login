2b785bc0cde4eff93653f6dc599a90880d1f695a
ShinigamiGod <kharrat_jaouher@hotmail.fr>
2014-05-19 22:05:20 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,60 @@
+    
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<script src="js/bootstrap.min.js"></script>
+<div class="alert alert-warning alert-dismissable">
+<i class="fa fa-exclamation-triangle"></i>construction.
+</div>
+    	<div class="holder">
+        <h5>Login / <a href="#">Sign up</a></h5>
+    	    <div class="col-xs-4 col-sm-2">
+		        <a href="#" class="btn btn-lg btn-block ">
+			        <i class="fa fa-facebook"></i>
+		        </a>
+	        </div>
+        	<div class="col-xs-4 col-sm-2">
+		        <a href="#" class="btn btn-lg btn-block ">
+			        <i class="fa fa-twitter"></i>
+		        </a>
+	        </div>	
+        	<div class="col-xs-4 col-sm-2">
+		        <a href="#" class="btn btn-lg btn-block ">
+			        <i class="fa fa-google-plus"></i>
+		        </a>
+	        </div>	
+<div class="clearfix"></div>
+	
+				
+			
+
+		<div class="row">
+			<div class="">	
+			    <form action="" autocomplete="off" method="POST">
+					<div class="input-group">
+						<span class="input-group-addon"><i class="fa fa-user"></i></span>
+						<input type="text" class="form-control" name="username" placeholder="" autofocus="true">
+					</div>
+					<span class="help-block"></span>
+										
+					<div class="input-group">
+						<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+						<input  type="password" class="form-control" name="password" placeholder="">
+					</div>
+                    <span class="help-block"></span>
+
+					<button class="btn btn-primary btn-block" type="submit">Login</button>
+				</form>
+			</div>
+    	</div>
+		<div>
+			<div>
+				<label class="checkbox">
+					<input type="checkbox" value="remember-me"><small>Remember Me</small>
+				</label>
+			</div>
+			<div class="col-xs-12 col-sm-3">
+				<p class="omb_forgotPwd">
+					<a href="#"><small>Forgot password?</small></a>
+				</p>
+			</div>
+		</div>	    	
+</div>
